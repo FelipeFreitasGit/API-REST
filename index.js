286d7a47e5f96c1208b2721dbb0d0a43aeadb795
FelipeFreitasGit <vasconcellosdefreitas@outlook.com>
2018-07-19 15:39:14 -0300

    criando schema mongg

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const app = require('express')();
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const dbConfig = require('./config/database');
 
 mongoose.connect(dbConfig.url);
+requireDir(dbConfig.modelsPath);
+
+const User = mongoose.model('User');
+User.create({
+  name: 'Diego',
+  username: 'felipe',
+  email: 'felipe@gmail.com',
+  password: '123',
+}, () => {
+  console.log('ok');
+});
 
 app.listen(3000);
