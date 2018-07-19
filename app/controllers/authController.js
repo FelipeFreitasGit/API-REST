7273e78e9a1ad451ddfbb1c2efe26de44372d23b
FelipeFreitasGit <vasconcellosdefreitas@outlook.com>
2018-07-19 17:45:36 -0300

    config login parte 1

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -3,6 +3,26 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
 module.exports = {
+  async signin(req, res, next) {
+    try {
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ error: 'User not found' });
+      }
+
+      if (!await user.compareHash(password)) {
+        return res.status(400).json({ error: 'Invalid password' });
+      }
+
+      return res.json(user);
+    } catch (err) {
+      return next(err);
+    }
+  },
+
   async signup(req, res, next) {
     try {
       const { email, username } = req.body;
